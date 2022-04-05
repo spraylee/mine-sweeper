e0b028a9dbf58bcfdea52db77516252a735809d2
sprayli <sprayli@tencent.com>
2022-04-05 18:29:44 +0800

    wip: 实现逻辑

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,154 @@
 <script setup lang="ts">
-const name = $ref('')
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
+const colors = [
+  'red',
+  'green',
+  'blue',
+  'yellow',
+  'purple',
+  'orange',
+  'pink',
+  'brown',
+]
+
+interface Box {
+  x: number;
+  y: number;
+  mine: boolean;
+  opened: boolean;
+  flagged: boolean;
+  nearMines: number;
 }
+class Game {
+  stage = ref([] as Box[][])
+  // 是否生成地雷了
+  isMinePlanted = false
+  state: 'playing' | 'win' | 'lose' = 'playing'
+  constructor(public size: number) {
+    this.size = size
+    this.reset()
+  }
+  reset() {
+    this.isMinePlanted = false
+    this.stage.value = []
+    for (let i = 0; i < this.size; i++) {
+      this.stage.value[i] = []
+      for (let j = 0; j < this.size; j++) {
+        this.stage.value[i][j] = {
+          y: i,
+          x: j,
+          mine: false,
+          opened: false,
+          flagged: false,
+          nearMines: 0
+        }
+      }
+    }
+  }
+  plantMines(count: number, ox: number, oy: number) {
+    if (this.isMinePlanted) return
+    this.isMinePlanted = true
+    const mines = []
+    while (mines.length < count) {
+      const x = Math.floor(Math.random() * this.size)
+      const y = Math.floor(Math.random() * this.size)
+      if (Math.abs(x - ox) <= 1 && Math.abs(y - oy) <= 1) continue
+      const mine = this.stage.value[x][y]
+      if (!mine.mine) {
+        mine.mine = true
+        mines.push(mine)
+      }
+    }
+    for (const mine of mines) {
+      const { x, y } = mine
+      for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+          if (i < 0 || i >= this.size || j < 0 || j >= this.size) continue
+          const nearMine = this.stage.value[i][j]
+          if (nearMine.mine) continue
+          nearMine.nearMines++
+        }
+      }
+    }
+  }
+  onBoxClick(x: number, y: number) {
+    const box = this.stage.value[y][x]
+    if (box.opened) return
+    // if (box.flagged) return
+
+    if (box.mine) {
+      this.state = 'lose'
+      return
+    } else {
+      box.opened = true
+    }
+    if (!this.isMinePlanted) {
+      this.plantMines(this.size ** 2 / 10, x, y)
+    }
+    // if (box.nearMines === 0) {
+    //   this.openNearBoxes(x, y)
+    // } else {
+    //   box.opened = true
+    // }
+    // if (this.isWin()) {
+    //   this.state = 'win'
+    // }
+  }
+}
+const game = new Game(10)
+
+const getBoxClass = (box: Box) => {
+  const classList = ['w-8', 'h-8', 'grid place-items-center']
+  classList.push('text-xs font-bold')
+  if (box.opened) {
+    classList.push('cursor-default bg-gray-400/50')
+  } else {
+    classList.push('cursor-pointer bg-gray-600 hover:bg-gray-300/20')
+  }
+  return classList
+}
+
+const stage = computed(() => game.stage.value)
+
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+  <main>
+    <h1>Mine Sweeper</h1>
+    <div class="game">
+      <div class="flex justify-center m10">
+        <div class>
+          <div class="flex" v-for="(row, y) in stage" :key="y">
+            <div
+              class="m-0.5"
+              v-for="(box, x) in row"
+              :key="x"
+              :class="getBoxClass(box)"
+              @click="game.onBoxClick(box.x, box.y)"
+            >
+              <div
+                v-if="box.opened && !box.mine && box.nearMines"
+                v-text="box.nearMines || ''"
+                :style="{ color: colors[box.nearMines] }"
+              ></div>
+              <div v-if="box.mine && box.opened">
+                <div i-carbon:close-filled></div>
+              </div>
+              <!-- <div class="cell-inner" v-if="cell.flagged" v-text="'🚩'"></div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="game-info">
+        <div class="status">
+          <span v-if="game.state === 'playing'">Playing...</span>
+          <span v-if="game.state === 'win'">You Win!</span>
+          <span v-if="game.state === 'lose'">You Lose!</span>
+        </div>
+        <div class="action">
+          <button v-if="game.state === 'playing'" @click="game.reset()">Reset</button>
+        </div>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
