77980fd02589efa5152e83f205fb7e0a812404ea
sprayli <sprayli@tencent.com>
2022-04-10 17:37:30 +0800

    feat: 模式选择，布局优化

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,15 @@
-<script setup lang="ts">
+<script setup lang="ts">import { isDark, toggleDark } from '~/composables';
+
 
 const colors: Record<number, string> = {
   1: '#3366FF',
   2: '#FF9933',
-  3: '#FFCC33',
-  4: '#33FF33',
-  5: '#33CCFF',
-  6: '#FF33CC',
-  7: '#FF3366',
-  8: '#FFCCCC',
+  3: '#FF6666',
+  4: '#FF2233',
+  5: '#FF1111',
+  6: '#FF0000',
+  7: '#FF0000',
+  8: '#FF0000',
 }
 
 interface Box {
@@ -75,7 +76,6 @@ class Game {
   }
   async onBoxClick(x: number, y: number) {
     if (this.loading) return
-    this.loading = true
     if (this.state !== 'playing') return
     if (!this.isMinePlanted) {
       this.plantMines(this.mines, x, y)
@@ -95,6 +95,7 @@ class Game {
       box.opened = true
       if (box.nearMines === 0) {
         let tempList: Box[] = [box]
+        this.loading = true
         while (tempList.length) {
           let list: Box[] = []
           for (const { x, y } of tempList) {
@@ -112,6 +113,7 @@ class Game {
           }
           tempList = list
         }
+        this.loading = false
       } else if (this.stage.flat().every(i => i.opened || i.mine)) {
         this.state = 'win'
         nextTick(() => {
@@ -119,7 +121,6 @@ class Game {
         })
       }
     }
-    this.loading = false
   }
   showAllMines() {
     for (const row of this.stage) {
@@ -132,8 +133,13 @@ class Game {
   }
 }
 
-const size = ref(10)
-const game = ref(new Game(size.value, size.value ** 2 / 10))
+const modes = [
+  { size: 8, mines: 8 },
+  { size: 10, mines: 12 },
+  { size: 14, mines: 20 },
+]
+const mode = useLocalStorage('game-mode', modes[1])
+const game = ref(new Game(mode.value.size, mode.value.mines))
 
 useStorage('state', game, undefined, {
   serializer: {
@@ -142,13 +148,22 @@ useStorage('state', game, undefined, {
   },
 })
 
+const changeMode = () => {
+  const index = modes.findIndex(i => i.size === game.value.size)
+  const nextMode = modes[(index + 1) % modes.length]
+  mode.value = nextMode
+  game.value.size = nextMode.size
+  game.value.mines = nextMode.mines
+  game.value.reset()
+}
+
 const getBoxClass = (box: Box) => {
   const classList = ['w-8 h-8 grid place-items-center']
   classList.push('text-xs font-bold')
   if (box.opened) {
-    classList.push('cursor-default bg-gray-400/10')
+    classList.push('cursor-default bg-gray-700/20')
   } else {
-    classList.push('cursor-pointer bg-gray-600 hover:bg-gray-300/20')
+    classList.push('cursor-pointer bg-gray-600 hover:bg-gray-600/80')
   }
   return classList
 }
@@ -157,13 +172,14 @@ const stage = computed(() => game.value.stage)
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
+
 </script>
 
 <template>
-  <main>
-    <h1>Mine Sweeper</h1>
-    <div class="game">
-      <div class="flex justify-center m10">
+  <main class="h-full grid justify-center content-center">
+    <h1 class="text-lg font-bold">扫雷</h1>
+    <div>
+      <div class="flex justify-center mt-6 mb-4">
         <!-- {{ game }} -->
         <div class>
           <div class="flex" v-for="(row, y) in stage" :key="y">
@@ -187,15 +203,22 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
           </div>
         </div>
       </div>
-      <div class="game-info">
+      <div class="flex items-center gap-4 border-t border-dashed border-gray-500/50 pt-3">
+        <span class="cursor-pointer" @click="changeMode">{{ game.size }} x {{ game.size }}</span>
+        <span class="flex items-center gap-1">
+          <i inline-block text-xs text-red-400 i-mdi:mine></i>
+          {{ game.mines }}
+        </span>
         <div class="status">
-          <span v-if="game.state === 'playing'">Playing...</span>
-          <span v-if="game.state === 'win'">You Win!</span>
-          <span v-if="game.state === 'lose'">You Lose!</span>
+          <span v-if="game.state === 'playing'">游戏中...</span>
+          <span text-green v-if="game.state === 'win'">你赢了!</span>
+          <span text-red v-if="game.state === 'lose'">你输了!</span>
         </div>
-        <div class="mt-2 flex justify-center">
-          <div class="cursor-pointer" @click="game.reset()" i-carbon:renew></div>
-        </div>
+        <div class="cursor-pointer ml-auto" @click="game.reset()" i-carbon:renew></div>
+        <button @click="toggleDark()">
+          <div v-if="isDark" i-carbon-moon />
+          <div v-else i-carbon-sun />
+        </button>
       </div>
     </div>
   </main>
